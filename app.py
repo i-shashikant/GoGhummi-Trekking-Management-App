da2594ff2e6b8b5a219d340606d76241c69c2455
Shashikant <shashi873kant@gmail.com>
2026-06-26 19:49:08 +0530

    Update Trek Database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from database import create_tables, add_user
+from database import create_tables, add_user, get_user_by_username
 
 app = Flask(__name__)
 
@@ -8,7 +8,16 @@ def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        print(f"Username: {username}, Password: {password}")
+        
+        user = get_user_by_username(username)
+        if user: 
+            if user[3] == password:
+                return render_template("dashboard.html")
+            else:
+                return "Invalid Password"
+        else: 
+            return "Username doesn't exists!"
+
     return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -17,10 +26,15 @@ def register():
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
+        existing_user = get_user_by_username(username)
+
+        if existing_user:
+            return "username already exists!"
+        
         add_user(username, email, password)
         return "User registered successfully!"
     return render_template("register.html")
 
+create_tables()
 if __name__ == "__main__":
-    create_tables()
     app.run(debug=True) 
--- database.py
+++ database.py
@@ -10,15 +10,34 @@ def create_tables():
     conn = get_connection()
     cursor = conn.cursor()
 
+    #Table for User
     cursor.execute("""
                CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT NOT NULL,
+                username TEXT NOT NULL UNIQUE,
                 email TEXT NOT NULL,
-                password TEXT NOT NULL
+                password TEXT NOT NULL,
+                role TEXT NOT NULL
             )
                """)
     
+    #Tables for treks
+    cursor.execute("""
+               CREATE TABLE IF NOT EXISTS treks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                trek_name TEXT NOT NULL UNIQUE,
+                location TEXT NOT NULL,
+                difficulty TEXT NOT NULL,
+                start_date TEXT NOT NULL,
+                end_date TEXT NOT NULL,
+                max_slots INTEGER NOT NULL,
+                available_slots INTEGER NOT NULL,
+                description TEXT,
+                status TEXT NOT NULL,
+                assigned_staffs TEXT
+            )
+               """)
+
     conn.commit()
     conn.close()
 
@@ -33,3 +52,16 @@ def add_user(username, email, password):
 
     conn.commit()
     conn.close()
+
+def get_user_by_username(username):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+               SELECT * FROM users WHERE username = ?
+           """, (username,))
+    user = cursor.fetchone()
+
+    conn.close()
+    return user
+
