37f390060695932e992746945ab637e5da9fa297
Shashikant <shashi873kant@gmail.com>
2026-06-29 22:24:04 +0530

    Staff Login Update

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
-from database import create_tables, add_user, get_user_by_username, add_trek, get_all_treks, delete_trek, get_trek_by_id, update_trek
+from flask import Flask, render_template, request, redirect, url_for, session
+from database import create_tables, add_user, get_user_by_username, add_trek, get_all_treks, delete_trek, get_trek_by_id, update_trek, get_pending_staff, approve_staff, get_approved_staff, get_treks_by_staff
 
 app = Flask(__name__)
+app.secret_key = "trekking_secret_key"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        
+
         user = get_user_by_username(username)
-        if user: 
+
+        if user:
+
             if user[3] == password:
-                return render_template("dashboard.html")
+                session["user_id"] = user[0]
+                session["username"] = user[1]
+                session["role"] = user[4]
+
+                if user[4] == "admin":
+                    return render_template("admin_dashboard.html")
+
+                elif user[4] == "user":
+                    return "User Dashboard Coming Soon"
+
+                elif user[4] == "staff":
+
+                    if user[5] == "Pending":
+                        return "Your account is waiting for admin approval."
+
+                    return "Staff Dashboard Coming Soon"
+
             else:
                 return "Invalid Password"
-        else: 
-            return "Username doesn't exists!"
+
+        else:
+            return "Username doesn't exist!"
 
     return render_template("login.html")
 
@@ -27,11 +47,16 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
         existing_user = get_user_by_username(username)
+        role = request.form.get("role")
 
         if existing_user:
             return "username already exists!"
-        
-        add_user(username, email, password)
+        if role == "staff":
+            approval_status = "Pending"
+        else:
+            approval_status = "Approved"
+
+        add_user(username, email, password, role, approval_status)
         return "User registered successfully!"
     return render_template("register.html")
 
@@ -74,6 +99,7 @@ def delete_trek_route(trek_id):
 @app.route("/edit_trek/<int:trek_id>", methods=["GET", "POST"])
 def edit_trek(trek_id):
     trek = get_trek_by_id(trek_id)
+    approved_staff = get_approved_staff()
     if request.method == "POST":
         trek_name = request.form.get("trek_name")
         location = request.form.get("location")
@@ -88,7 +114,27 @@ def edit_trek(trek_id):
 
         update_trek(trek_id, trek_name, location, difficulty, start_date, end_date, duration, max_slots, description, status, assigned_staff_id)
         return redirect(url_for("view_treks"))
-    return render_template("edit_trek.html", trek=trek)
+    approved_staff = get_approved_staff()
+    return render_template("edit_trek.html", trek=trek, approved_staff=approved_staff)
+
+@app.route("/pending_staff")
+def pending_staff():
+    staff = get_pending_staff()
+    return render_template("pending_staff.html", staff=staff)
+
+@app.route('/approve_staff/<int:staff_id>')
+def approve_staff_route(staff_id):
+    approve_staff(staff_id)
+    return redirect(url_for("pending_staff"))
+
+
+
+
+
+
+
+
+
 
 create_tables()
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,13 +1,18 @@
 import sqlite3
+import os
 
 
 def get_connection():
     conn = sqlite3.connect('trek.db')
-    cursor = conn.cursor()
     return conn
+# def get_connection():
+#     print("Using database:", os.path.abspath("trek.db"))
+
+#     conn = sqlite3.connect("trek.db")
+#     return conn
 
 def create_tables():
-    conn = get_connection()
+    conn = get_connection() 
     cursor = conn.cursor()
 
     #Table for User
@@ -16,7 +21,9 @@ def create_tables():
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL UNIQUE,
                 email TEXT NOT NULL UNIQUE,
-                password TEXT NOT NULL
+                password TEXT NOT NULL,
+                role TEXT NOT NULL,
+                approval_status TEXT NOT NULL
             )
                """)
     
@@ -40,17 +47,18 @@ def create_tables():
     conn.commit()
     conn.close()
 
-def add_user(username, email, password):
+def add_user(username, email, password, role, approval_status):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-               INSERT INTO users (username, email, password)
-               VALUES (?, ?, ?)
-           """, (username, email, password))
+               INSERT INTO users (username, email, password, role, approval_status)
+               VALUES (?, ?, ?, ?, ?)
+           """, (username, email, password, role, approval_status))
 
     conn.commit()
     conn.close()
+
 
 def get_user_by_username(username):
     conn = get_connection()
@@ -131,3 +139,51 @@ def update_trek(trek_id, trek_name, location, difficulty, start_date, end_date,
 
     conn.commit()
     conn.close()
+
+def get_pending_staff():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   SELECT * FROM users WHERE role = 'staff' AND approval_status = 'Pending'
+               """)
+    pending_staff = cursor.fetchall()
+
+    conn.close()
+    return pending_staff
+
+
+def approve_staff(staff_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   UPDATE users SET approval_status = ? WHERE id = ?
+               """, ('Approved', staff_id))
+
+    conn.commit()
+    conn.close()
+
+def get_approved_staff():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   SELECT * FROM users WHERE role = 'staff' AND approval_status = 'Approved'
+               """)
+    approved_staff = cursor.fetchall()
+
+    conn.close()
+    return approved_staff
+
+def get_treks_by_staff(staff_id):
+    conn = get_connection()
+    cursor = conn.cursor
+
+    cursor.execute(""""
+                   SELECT * from treks WHERE assigned_staff_id = ? 
+                   """, (staff_id,))
+    treks = cursor.fetchall()
+    conn.close
+    return treks
+
